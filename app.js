2fbd7d8ac73dd41718914577af43867a6829943a
TylerAStephens <tylerstephens16@yahoo.com>
2020-08-08 14:39:33 -0400

    set up GET request

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.listen(3000, function() {
+  console.log("Local Server on 3000");
+});
