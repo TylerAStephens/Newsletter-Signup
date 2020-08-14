0a74ffd733058a8359921511116b6b4308c03203
TylerAStephens <tylerstephens16@yahoo.com>
2020-08-13 21:58:15 -0400

    finished api logic and added gitignore

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+
+require('dotenv').config()
+
+console.log(process.env);
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 app.get("/", function(req, res) {
@@ -14,10 +21,38 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
   console.log(firstName, lastName, email);
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName,
+      }
+    }]
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us17.api.mailchimp.com/3.0/lists/" + process.env.LIST_ID;
+
+  const options = {
+    method: "POST",
+    auth: process.env.API_KEY
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
 
 });
 
